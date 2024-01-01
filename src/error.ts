488945cc9b3a155859ff71d717e7a1c962f61653
bya2 <byaa1972@gmail.com>
2024-01-01 14:13:13 +0900

    구현 방식 변경으로 인한 전체 소스 코드 삭제

--- src/error.ts
+++
@@ -1,8 +0,0 @@
-export function UnwrapFailed<E = any>(msg: string = "Unwrap failed", err?: E): never {
-  if (err !== undefined) msg = `${msg}\n${err}`;
-  throw new Error(msg);
-}
-
-export function UnreachableCodeExecuted(msg = "Unreachable code executed"): never {
-  throw new Error(msg);
-}
